aa912fc72cd9c290ffe9c99539c081efa6cad315
Ramon <ramonmulia@gmail.com>
2018-06-28 03:44:46 -0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/totvs-labs-challenge'));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
